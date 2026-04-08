d4393e617332a92243bc8e659008a121d6fa2126
fortinnael93-hub <fortinnael93@gmail.com>
2026-04-08 18:29:59 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -22,7 +22,8 @@ app.get('/', (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`✅ API démarrée sur le port ${PORT}`);
 });
 
