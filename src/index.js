d229469efb44f0e0b75a1e2fbf174f1483aed3ac
fortinnael93-hub <fortinnael93@gmail.com>
2026-04-17 17:59:01 +0200

    Add express.text middleware for application/json type

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const app        = express();
 
 app.use(cors());
 app.use(express.text());
+app.use(express.text({ type: 'application/json' }));  
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
